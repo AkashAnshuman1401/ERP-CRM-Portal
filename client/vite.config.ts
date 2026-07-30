9fe093de3e1081fc976ad102431a11ff1fb079e0
Virat Paglu <Arnav1401singh@gmail.com>
2026-07-30 22:00:17 +0530

    fixed the error

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const isProd = mode === "production";
+  return {
+    base: isProd ? "/ERP-CRM-Portal/" : "/",
+    plugins: [react()],
+  };
 });
